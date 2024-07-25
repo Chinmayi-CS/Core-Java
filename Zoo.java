b42a0000b783db30086d6127631be11151eb8458
Chinmayi-CS <chinmayics.xworkz@gmail.com>
2024-07-25 02:40:23 -0700

    Second file

---
+++ Zoo.java
@@ -0,0 +1,13 @@
+//zoo-cageAnimal-Officer-openGate
+//ZooRunner--main()--Zoo.cageAnimal
+
+
+class Zoo
+{
+	public static void cageAnimal()
+	{
+		System.out.println("Running cageAnimal in zoo");
+		Officer.openGate();
+	}
+	
+}
