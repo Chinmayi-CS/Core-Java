584a99c3e4ca825e02ae7936a39feec897503512
Chinmayi-CS <chinmayics.xworkz@gmail.com>
2024-07-25 02:31:06 -0700

    first file

---
+++ School.java
@@ -0,0 +1,21 @@
+//calling method from another class
+//declare a class,declare 5 static methods
+//declare a class with main method and call all 5 methods from above class
+
+class School{
+static void startSchool(){
+	System.out.println("School has started for the day");
+}
+static void morningAssembly(){
+	System.out.println("Morning assembly is in progress");
+}
+static void classesInSession(){
+	System.out.println("classes are now in session");
+}
+static void lunchBreak(){
+	System.out.println("It's lunch break time");
+}
+static void endSchool(){
+	System.out.println("School has ended for the day");
+}
+}
